2a395343a86772dfc8266d07fa31634eeb4f55d7
tgparkk <45591830+tgparkk@users.noreply.github.com>
2022-12-09 13:57:40 +0900

    Create ThreadPool.cpp

---
+++ ThreadPoolTest/ThreadPool.cpp
@@ -0,0 +1,66 @@
+#include "ThreadPool.h"
+
+ThreadPool::ThreadPool(size_t num_threads)
+	: num_threads_(num_threads), stop_all(false) 
+{
+	worker_threads_.reserve(num_threads_);
+	for (size_t i = 0; i < num_threads_; ++i) 
+	{
+		worker_threads_.emplace_back([this]() { this->WorkerThread(); });
+	}
+}
+
+ThreadPool::~ThreadPool() 
+{
+	stop_all = true;
+	cv_job_q_.notify_all();
+
+	for (auto& t : worker_threads_) 
+	{
+		t.join();
+	}
+}
+
+void ThreadPool::WorkerThread() {
+	while (true) 
+	{
+		// std::lock_guard 는 lock과 unlock 사이에 lock,unlock 사용 불가
+		std::unique_lock<std::mutex> lock(m_job_q_);
+
+		cv_job_q_.wait(lock, [this]() { return !jobs_.empty() || stop_all; });
+		// 1) Lock을 잡은 상태
+		// 2) 조건 확인
+		//	- 만족 O => 빠져 나와서 이어서 코드를 진행(락을 획득하고 탈출)
+		//	- 만족 X => Lock을 원자적으로 풀고 블락됨(wait(loci))
+		//		a) notify_xxx()가 호출되면 스레드가 깨어난다
+		//		b) lock을 다시 얻고(wait(lock), 2)부터 반복
+
+
+		if (stop_all && jobs_.empty())
+		{
+			return;
+		}
+
+		// 맨 앞의 job 을 뺀다.
+		std::function<void()> job = std::move(jobs_.front());
+		jobs_.pop();
+		lock.unlock();
+
+		// 해당 job 을 수행한다 :)
+		job();
+	}
+}
+
+void ThreadPool::EnqueueJob(std::function<void()> job) {
+	if (stop_all) 
+	{
+		throw std::runtime_error("ThreadPool 사용 중지됨");
+	}
+	{
+		std::lock_guard<std::mutex> lock(m_job_q_);
+		jobs_.push(std::move(job));
+	}
+	
+	// 대기하고 있는 하나의 thread 깨우기
+	cv_job_q_.notify_one();
+}
