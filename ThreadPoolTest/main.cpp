38e474235abed295d6168161ce50d1d9f3279593
tgparkk <45591830+tgparkk@users.noreply.github.com>
2022-12-09 13:56:39 +0900

    Create main.cpp

---
+++ ThreadPoolTest/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "ThreadPool.h"
+#include "Task1.h"
+
+
+int main()
+{
+	std::cout << "test" << std::endl;
+
+	ThreadPool pool(3);
+
+	for (int i = 0; i < 10; i++) 
+	{
+// 		pool.EnqueueJob([i]() 
+// 			{ 
+// 				Task1 t1;
+// 				t1.work(i % 3 + 1, i); 
+// 			});
+	}
+
+	return 0;
+}
