3ceb02c16d2950e1e4ac4fe5b55f3582a99dea06
tgparkk <45591830+tgparkk@users.noreply.github.com>
2022-12-27 15:23:24 +0900

    Update dataloading.cpp

--- test/dataloading.cpp
+++ test/dataloading.cpp
@@ -71,3 +71,87 @@ void FirstDataLoader::load_polyline(AsyncTcpClient& client)
 
 
 }
+
+
+
+
+void FirstDataLoader::OnRequestSelectPolylineNew(const IMessage* msg, const boost::system::error_code& ec)
+{
+	if (ec) {
+		MessageBoxA(NULL, ec.message().c_str(), __FUNCTION__, MB_OK);
+		return;
+	}
+
+	if (msg->msg_id() == eNetID::Res_Error_V1) {
+		MessageBoxW(NULL, msg->to_string().c_str(), _T(__FUNCTION__), MB_OK);
+		return;
+	}
+
+	if (msg->msg_id() != eNetID::Res_LoadPolylines_V1)
+	{
+		_ASSERT(false);
+		return;
+	}
+
+	const ResLoadPolylines_V1* response = dynamic_cast<const ResLoadPolylines_V1*>(msg);	
+
+	CGeoObjectList objs;
+
+	const std::vector<out::Polyline>& datas = response->getdatas();
+
+	for (int i = 0 ;i < (int)datas.size(); i++)
+	{
+		const out::Polyline& data = datas[i];
+
+		CGeoPolyline* pPolyline = new CGeoPolyline();
+
+		UNIONID	unionID;
+		unionID.dwObjectID = data.m_id;
+		unionID.strMapID = ConvMapIDDword2String(data.m_mapid);
+		
+		pPolyline->SetObjID( unionID );
+
+		pPolyline->SetKind(data.m_kind);
+		
+		wchar_t w_bes[out::Polyline::ID_LEN] = {0, };
+		conv_UTF16(&data.m_bdgid_bes[0], sizeof(data.m_bdgid_bes), w_bes, sizeof(w_bes));
+		pPolyline->Set3DBdgID(w_bes, BESSEL); //Cstring 내부에서 utf16변환
+		
+		wchar_t w_grs[out::Polyline::ID_LEN] = {0, };
+		conv_UTF16(&data.m_bdgid_grs[0], sizeof(data.m_bdgid_grs), w_grs, sizeof(w_grs));
+
+		pPolyline->Set3DBdgID(w_grs, GRS80); //CString 내부에서 utf16변환
+
+		if (!data.m_xy_bes.empty()) {
+			pPolyline->ConvWKBToGeoType((BYTE*)&data.m_xy_bes[0], BESSEL);
+		}
+
+		if (!data.m_xy_grs.empty()) {
+			pPolyline->ConvWKBToGeoType((BYTE*)&data.m_xy_grs[0], GRS80);
+		}
+		
+		pPolyline->SetSyncFlag((BYTE)data.m_syncflag);
+
+		objs.Add( pPolyline );
+	}
+
+	if (eLoadVerifyTest::Use)
+	{
+		CGeoObjectList objsB;
+		App::GetDataCommManager().GetPolylineDataByMapID(&objsB, m_strMapID);
+		if (!util::is_equal_test(eEqualTest::polyline, objs, objsB)) 
+		{
+			ASSERT(false); //검증실패
+			AELOG("결과값이 다릅니다.");
+		}
+	}
+	
+	CKairosDBManager& kairosman = App::GetKairosDBManager();
+	CGeoObjectTable* pGeoTable = kairosman.CreateObjectTable(POLYLINE_OBJECT);
+	if (pGeoTable)
+	{
+		objs.deleteList(pGeoTable->GetObjList());
+		pGeoTable->lm_AddList(objs);
+		pGeoTable->QSort();
+	}
+}
